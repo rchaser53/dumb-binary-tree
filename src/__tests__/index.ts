19646192022f88a33a4908d0fb851c9f5e887d0d
rchaser53 <tayoshizawa29@gmail.com>
2020-06-06 12:44:36 +0900

    set up

---
+++ src/__tests__/index.ts
@@ -0,0 +1,52 @@
+describe("test", () => {
+  type Tree = {
+    left: Tree | undefined;
+    right: Tree | undefined;
+    val: any;
+  };
+  const createTree = (val: any, left?: Tree, right?: Tree): Tree => {
+    return {
+      val,
+      left,
+      right,
+    };
+  };
+
+  const binary = (input: any[]): Tree | undefined => {
+    if (input.length === 0) return;
+
+    const dummy = createTree(input.shift());
+    let root = dummy;
+    let baseStack = [root];
+    while (0 < input.length || 0 < baseStack.length) {
+      const stack = [];
+      while (0 < baseStack.length) {
+        const node = baseStack.shift();
+        const left = input.shift();
+        const right = input.shift();
+
+        if (left != null) {
+          node.left = createTree(left);
+          stack.push(node.left);
+        }
+
+        if (right != null) {
+          node.right = createTree(right);
+          stack.push(node.right);
+        }
+      }
+      baseStack = stack;
+    }
+
+    return dummy;
+  };
+
+  test("basic", () => {
+    const node = binary([1, 2, 3]);
+    expect(node).toEqual({
+      left: createTree(2),
+      right: createTree(3),
+      val: 1,
+    });
+  });
+});
