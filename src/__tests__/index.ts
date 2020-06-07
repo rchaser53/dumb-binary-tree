1ab92502e064be49eca3a89747e68d2ebb592833
rchaser53 <tayoshizawa29@gmail.com>
2020-06-07 15:12:57 +0900

    add test case

--- src/__tests__/index.ts
+++ src/__tests__/index.ts
@@ -7,6 +7,22 @@ describe("test", () => {
       right: createTree(3),
       val: 1,
     });
+
+    expect(binary([1, 2, 3, null, 4, 5, null])).toEqual({
+      left: createTree(2, undefined, createTree(4)),
+      right: createTree(3, createTree(5)),
+      val: 1,
+    });
+
+    expect(binary([1, 2, 3, null, 4, null, null, 5, null, 6])).toEqual({
+      left: createTree(
+        2,
+        undefined,
+        createTree(4, createTree(5, createTree(6)))
+      ),
+      right: createTree(3),
+      val: 1,
+    });
   });
 
   test("return undefined when input is empty array", () => {
